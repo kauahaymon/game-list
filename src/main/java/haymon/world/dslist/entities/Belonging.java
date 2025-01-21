642cda8a42b44c8eaa4058471bed69e9d61a8ce1
Kauã Haymon <kauahaymon@hotmail.com>
2025-01-21 15:14:50 -0400

    feat: domain model

---
+++ src/main/java/haymon/world/dslist/entities/Belonging.java
@@ -0,0 +1,40 @@
+package haymon.world.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+	
+	@EmbeddedId
+	private BelongingPK id = new BelongingPK();
+	private Integer position;
+	
+	public Belonging() {}
+	
+	public Belonging(Game game, GameList gameList, Integer position) {
+		id.setGame(game);
+		id.setGameList(gameList);
+		this.position = position;
+	}
+
+	public BelongingPK getId() {
+		return id;
+	}
+
+	public void setId(BelongingPK id) {
+		this.id = id;
+	}
+
+	public Integer getPosition() {
+		return position;
+	}
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+	
+	
+}
