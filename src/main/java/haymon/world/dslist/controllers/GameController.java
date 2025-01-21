99bdf7ec377a653ff1cfab05b11ee9e7bc9b1a84
Kauã Haymon <kauahaymon@hotmail.com>
2025-01-21 16:36:54 -0400

    add: GameDTO, get game by id

--- src/main/java/haymon/world/dslist/controllers/GameController.java
+++ src/main/java/haymon/world/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import haymon.world.dslist.dto.GameDTO;
 import haymon.world.dslist.dto.GameMinDTO;
 import haymon.world.dslist.entities.Game;
 import haymon.world.dslist.services.GameService;
@@ -23,5 +25,10 @@ public class GameController {
 		List<Game> games = gameService.findAll();
 		List<GameMinDTO> dto = games.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO getById(@PathVariable Long id) {
+		 return gameService.findById(id);
 	}
 }
--- src/main/java/haymon/world/dslist/services/GameService.java
+++ src/main/java/haymon/world/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import haymon.world.dslist.dto.GameDTO;
 import haymon.world.dslist.entities.Game;
 import haymon.world.dslist.repositories.GameRepository;
 
@@ -14,7 +16,15 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional
 	public List<Game> findAll() {
 		return gameRepository.findAll();
+	}
+	
+	@Transactional
+	public GameDTO findById(Long id) {
+		Game game = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(game);
+		return dto;
 	}
 }
